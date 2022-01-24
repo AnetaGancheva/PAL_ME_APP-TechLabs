59a806ef120402afe1ac406ad908161f3f5a12b7
Arwa Ashawy <arwa.ashawy@gmail.com>
2022-01-24 12:43:45 +0100

    designed a button component for downloading the app in the home page

---
+++ pal-me/src/components/StyledButton.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components'
+import {FaApple} from 'react-icons/fa'
+const Button = styled.button`
+    background-color: teal;
+    border-radius: 4px;
+    color: white;
+    width: 140px;
+    height: 50px;
+    border: none;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    transition: all 0.5s ease-in-out;
+
+
+    &:hover{
+        transform: translateY(-4px) ;
+    }
+`
+
+const StyledButton = ({appName, appSymbol}) => {
+  return <Button>
+      <p style={{textAlign: 'center', fontWeight:'bold', fontSize:"25px"}}>{appSymbol}</p>  
+      <p>Get it Free at 
+        <span style={{display:"block", fontWeight: 'bold'}}>{appName}</span></p>  
+      </Button>;
+};
+
+export default StyledButton;
--- pal-me/src/pages/Home.jsx
+++ pal-me/src/pages/Home.jsx
@@ -1,14 +1,46 @@
 import React from 'react'
+import { useContext } from 'react'
 import Navbar from '../components/Navbar'
-import Login from './Login'
-import Register from './Register'
+import styled from "styled-components"
+import bgImg from '../images/homebg.jpg'
+import StyledButton from '../components/StyledButton'
+import {FaApple, FaGooglePlay} from 'react-icons/fa'
+import { GlobalContext } from '../Context'
 
+
+const Container = styled.div`
+    width: 100%;
+    /* if user is Logged ,height will equal 100vh - (8vh)navbar height - (5vh)user navbar height*/
+    height: ${props => props.isLogged ?  "87vh" : "92vh"};
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    background-color: red ;
+`
+
+const Wrapper = styled.div`
+    background-color: blue ;
+    /* width: 100%; */
+    padding: 3%;
+`
 const Home = () => {
+
+    const {isLogged} = useContext(GlobalContext)
     return (
-        <div>
+        <>
             {/* <Register/> */}
             <Navbar/>
-        </div>
+            <Container isLogged={isLogged}>
+                <Wrapper>
+                    <Desc>
+
+                    </Desc>
+                </Wrapper>
+
+
+                
+            </Container>
+        </>
     )
 }
 
